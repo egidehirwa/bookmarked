25b9e2286e5a29bde1aa51e275405bad5dfaad07
egidehirwa <egidehirwam@gmail.com>
2024-07-02 23:18:57 +0200

    fix: debut tailwind and add shadcn-ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <section className='bg-slate-500'>
+      <section className='bg-red-300'>
         <h1 className="text-4xl font-bold underline">Let the bookmarking begin.</h1>
       </section>
     </>
