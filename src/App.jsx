d8c13d393a272eb09c93665a0b624d4d6498c96f
egidehirwa <egidehirwam@gmail.com>
2024-07-04 23:56:51 +0200

    Combining components to work together

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './index.css';
 import './App.css'
 import Header from './components/Header';
+import Select from './components/Select';
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Select></Select>
     </>
   )
 }
